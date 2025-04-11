c986c31abcd6acb5b29604bc53c1bf7cb0d46656
Artyom <delangame666@gmail.com>
2025-04-11 13:18:07 +0300

    add check places

--- app/callbacks/pag_regs.py
+++ app/callbacks/pag_regs.py
@@ -3,6 +3,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 
 from app.database.models.region import Region
+from app.keyboards.builders import create_inline_keyboard
+from app.services.place_service import PlaceService
 from app.services.region_service import RegionService
 from app.utils.logger import logger
 from app.utils.pagination import Pagination
@@ -10,31 +12,68 @@ from app.utils.pagination import Pagination
 router = Router()
 
 
+@router.callback_query(F.data == "back")
+async def go_to_back(callback: CallbackQuery):
+    await callback.message.delete()
+
+
+@router.callback_query(F.data.startswith("places:reg:"))
+async def get_regions(
+        callback: CallbackQuery,
+        state: FSMContext,
+        place_service: PlaceService,
+):
+    place_id = int(callback.data.split(":")[-1])
+
+    place_info = await place_service.get_one_place(place_id)
+
+    await callback.message.answer(
+        text=f"{place_info.title}\n"
+             f"Описание:\n"
+             f"{place_info.description}",
+        reply_markup=await create_inline_keyboard(
+            [("Назад", "back")]
+        )
+    )
+
+
 @router.callback_query(F.data.startswith("regions:reg:"))
 async def get_regions(
         callback: CallbackQuery,
         region_service: RegionService,
         state: FSMContext,
+        place_service: PlaceService,
 ):
-    user_id = callback.from_user.id
-    user_pagination = await state.get_value("user_pagination", None)
+    try:
+        user_id = callback.from_user.id
+        user_pagination = await state.get_value("user_pagination", None)
 
-    if user_id not in user_pagination or user_pagination is None:
-        await callback.answer("❌ Сессия устарела! Зайдите в список регионов заново")
-        return
+        if user_id not in user_pagination or user_pagination is None:
+            await callback.answer("❌ Сессия устарела! Зайдите в список регионов заново")
+            return
 
-    region_id = int(callback.data.split(":")[-1])
+        region_id = int(callback.data.split(":")[-1])
 
-    region_info = await region_service.get_one_region(region_id)
+        region_info = await region_service.get_one_region(region_id)
 
-    await callback.message.answer(
-        text=f"{region_info.name}\n\n"
-             f"Описание:\n"
-             f"{region_info.description}",
-        # reply_markup=
-    )
+        region_places = await place_service.get_all_by_region_id(region_id)
 
-    await callback.answer(f"Выбран регион {region_id}")
+        pagination = Pagination(region_places)
+        user_pagination[callback.from_user.id] = pagination
+
+        keyboard = await pagination.get_page_keyboard(prefix="places")
+
+        await state.update_data(user_pagination=user_pagination)
+
+        await callback.message.answer(
+            text=f"{region_info.name}\n\n"
+                 f"Описание:\n"
+                 f"{region_info.description}\n\n"
+                 f"Места:",
+            reply_markup=keyboard
+        )
+    except Exception as e:
+        logger.error(f"Ошибка в пагинации мест: {e}")
 
 
 @router.callback_query(F.data.startswith("regions:"))
