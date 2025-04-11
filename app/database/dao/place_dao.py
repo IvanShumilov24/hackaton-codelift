1d6b03697a36bbb6ab585da47047cfc1c2168156
ulver <ivansergshum@gmail.com>
2025-04-11 13:34:49 +0300

    fix

--- app/database/dao/place_dao.py
+++ app/database/dao/place_dao.py
@@ -3,7 +3,6 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.models.place import Place
-from app.database.models.region import Region
 from app.utils.logger import logger
 
 
@@ -13,7 +12,7 @@ class PlaceDAO:
 
     async def get_one_or_none(self, place_id: int) -> Place | None:
         try:
-            query = select(Region).where(Region.region_id == place_id)
+            query = select(Place).where(Place.place_id == place_id)
             result = await self.session.execute(query)
             return result.scalar_one_or_none()
         except SQLAlchemyError as e:
--- app/services/place_service.py
+++ app/services/place_service.py
@@ -1,4 +1,3 @@
-from app.database.dao import place_dao
 from app.database.dao.place_dao import PlaceDAO
 from app.database.models.place import Place
 from app.utils.logger import logger
@@ -10,7 +9,7 @@ class PlaceService:
 
     async def get_all_by_region_id(self, region_id: int) -> list[Place]:
         try:
-            all_places = await place_dao.get_all()
+            all_places = await self.place_dao.get_all()
             places_by_region = [place for place in all_places if place.region_id == region_id]
             logger.info(f"Найдено {len(places_by_region)} мест региона")
             return places_by_region
@@ -20,7 +19,7 @@ class PlaceService:
 
     async def get_one_place(self, place_id: int) -> Place:
         try:
-            place = await place_dao.get_one_or_none(place_id)
+            place = await self.place_dao.get_one_or_none(place_id)
             if not place:
                 raise ValueError("Место не найдено")
             return place
