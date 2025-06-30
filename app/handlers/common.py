35032a4bde043976c414b9d986ead4fa1af6b6d8
Artyom <delangame666@gmail.com>
2025-06-30 19:37:57 +0300

    fix main_menu bug

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -17,15 +17,31 @@ router = Router()
 async def start_handler(
         message: Message,
         user_service: UserService,
+        is_edited: bool = False,
+        user_id: int = None,
+        first_name: str = None,
 ):
     try:
-        await user_service.register_user(
-            user_id=int(message.from_user.id),
-            first_name=message.from_user.first_name,
-        )
+        if user_id and first_name:
+            await user_service.register_user(
+                user_id=int(user_id),
+                first_name=first_name,
+            )
+        else:
+            await user_service.register_user(
+                user_id=int(message.from_user.id),
+                first_name=message.from_user.first_name,
+            )
+
+        if is_edited:
+            await message.edit_text(
+                text=f"Приветствуем тебя в нашем путеводителе по ЛО",
+                reply_markup=await create_inline_keyboard([("Перейти к списку", "regions")])
+            )
+            return
 
         await message.answer(
-            text=f"Привет {message.from_user.first_name}! Приветствуем тебя в нашем путеводителе по ЛО",
+            text=f"Приветствуем тебя в нашем путеводителе по ЛО",
             reply_markup=await create_inline_keyboard([("Перейти к списку", "regions")])
         )
 
@@ -39,4 +55,10 @@ async def main_menu_handler(
         callback: CallbackQuery,
         user_service: UserService,
 ):
-    await start_handler(message=callback.message, user_service=user_service)
+    await start_handler(
+        message=callback.message,
+        user_service=user_service,
+        is_edited=True,
+        user_id=callback.from_user.id,
+        first_name=callback.from_user.first_name,
+    )
