c88d1221a8588248fded525e4d1ad2d15066d4c8
Artyom <delangame666@gmail.com>
2025-06-30 19:41:55 +0300

    refactoring common.py

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -1,64 +1,54 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 
-from app.database.models.region import Region
 from app.keyboards.builders import create_inline_keyboard
-from app.services.region_service import RegionService
 from app.services.user_service import UserService
 from app.utils.logger import logger
-from app.utils.pagination import Pagination
 
 router = Router()
 
 
-@router.message(CommandStart())
-async def start_handler(
+async def handle_user_start(
         message: Message,
         user_service: UserService,
+        user_id: int,
+        first_name: str,
         is_edited: bool = False,
-        user_id: int = None,
-        first_name: str = None,
 ):
     try:
-        if user_id and first_name:
-            await user_service.register_user(
-                user_id=int(user_id),
-                first_name=first_name,
-            )
-        else:
-            await user_service.register_user(
-                user_id=int(message.from_user.id),
-                first_name=message.from_user.first_name,
-            )
+        await user_service.register_user(user_id=user_id, first_name=first_name)
+
+        text = "Приветствуем тебя в нашем путеводителе по ЛО"
+        keyboard = await create_inline_keyboard([("Перейти к списку", "regions")])
 
         if is_edited:
-            await message.edit_text(
-                text=f"Приветствуем тебя в нашем путеводителе по ЛО",
-                reply_markup=await create_inline_keyboard([("Перейти к списку", "regions")])
-            )
-            return
-
-        await message.answer(
-            text=f"Приветствуем тебя в нашем путеводителе по ЛО",
-            reply_markup=await create_inline_keyboard([("Перейти к списку", "regions")])
-        )
+            await message.edit_text(text=text, reply_markup=keyboard)
+        else:
+            await message.answer(text=text, reply_markup=keyboard)
 
     except Exception as e:
         logger.error(f"Ошибка при обработке /start: {e}")
         await message.answer("⚠️ Произошла ошибка при регистрации. Попробуйте позже.")
 
 
+@router.message(CommandStart())
+async def start_handler(message: Message, user_service: UserService):
+    await handle_user_start(
+        message=message,
+        user_service=user_service,
+        user_id=message.from_user.id,
+        first_name=message.from_user.first_name
+    )
+
+
 @router.callback_query(F.data == "main_menu")
-async def main_menu_handler(
-        callback: CallbackQuery,
-        user_service: UserService,
-):
-    await start_handler(
+async def main_menu_handler(callback: CallbackQuery, user_service: UserService):
+    await handle_user_start(
         message=callback.message,
         user_service=user_service,
-        is_edited=True,
         user_id=callback.from_user.id,
         first_name=callback.from_user.first_name,
+        is_edited=True
     )
+
